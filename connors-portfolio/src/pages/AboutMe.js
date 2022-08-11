3a1fe1ba3a8c3c2721a279358224dfc83fbbb969
Rare Raptor <connorwtuncc@gmail.com>
2022-08-10 21:39:26 -0400

    finished adding content to resume, added an about me paragraph

--- connors-portfolio/src/pages/AboutMe.js
+++ connors-portfolio/src/pages/AboutMe.js
@@ -2,10 +2,15 @@ import React from "react";
 
 const About = () => {
     return (
-        <div style={{ background: 'rgb(24, 24, 24)', height: '100vh'}}>
-            <h1 className="d-flex justify-content-center pt-4" style={{ color: 'rgb(200, 200, 200)'}}>
+        <div style={{ background: 'rgb(24, 24, 24)', height: '100vh', color: 'rgb(200, 200, 200)' }}>
+            <h1 className="d-flex justify-content-center pt-4">
                 About Me
             </h1>
+            <p style={{ textAlign: 'center', paddingLeft: '20%', paddingRight: '20%'}}>
+                My name is Connor Thompson, I'm a junior web developer based in charlotte NC, USA.
+                I'm passionate about building cutting-edge applications! My journey in coding started
+                a year ago and I've been striving to build bigger and better projects everyday.
+            </p>
         </div>
     )
 };
--- connors-portfolio/src/pages/Resume.js
+++ connors-portfolio/src/pages/Resume.js
@@ -14,9 +14,9 @@ const Resume = () => {
             </p>
             <hr className="m-auto w-50" />
             <section className="container mb-5" style={{ paddingLeft: '17%' }}>
-                <h5 className="pt-4">
+                <h4 className="pt-4">
                     Summary
-                </h5>
+                </h4>
                 <p>
                     Efficient Junior Web Developer with Certificate in Full Stack Web Development. Excellent
                     communication and organizational abilities with resourceful approach to solving diverse problems.
@@ -25,32 +25,140 @@ const Resume = () => {
                     Collaborative and hardworking team player with critical thinking skills and systematic approach.
                 </p>
                 <hr className="w-75" />
-                <h5>
+                <h4>
                     Skills
-                </h5>
+                </h4>
                 <p>
                     React, JavaScript, CSS, HTML, Node, Express, MySQL, NoSQL, MongoDB, Bootstrap, Client-Side Routing,
                     Authentication, User Experience Design (UX)
                 </p>
                 <hr className="w-75" />
-                <h5>
+                <h4>
                     Education
-                </h5>
-                <p className="">
-                    Highschool Diploma
+                </h4>
+                <ul>
+                    <p className="">
+                        Highschool Diploma
 
-                </p>
-                <p>
-                    Weddington HighSchool
+                    </p>
+                    - Weddington HighSchool
                     2013-2017
-                </p>
+                    <p className="pt-4">
+                        Certificate: Full Stack Web Development
+                    </p>
+                    <p>
+                        - University of North Carolina at Charlotte
+                        05/2022-08/2022
+                    </p>
+                </ul>
+                <hr className="w-75" />
+                <h4>
+                    Work Experience
+                </h4>
                 <p>
-                    Certificate: Full Stack Web Development
+                    DSP Associate - Spears Enterprises LLC
                 </p>
+                <ul>
+                    <li>
+                        Completed on-time deliveries by choosing best and most efficient routes.
+                    </li>
+                    <li>
+                        Operated vehicle safely in highly congested areas with no traffic violations.
+                    </li>
+                    <li>
+                        Completed rush deliveries on tight timetables to satisfy customer needs.
+                    </li>
+                    <li>
+                        Communicated customer complaints, requests and feedback to company management for
+                        swift resolution.
+                    </li>
+                    <li>
+                        Contacted customers prior to delivery to confirm and coordinate delivery times.
+                    </li>
+                </ul>
                 <p>
-                    University of North Carolina at Charlotte
-                    05/2022-08/2022
+                    HVAC Technician - Mount Mitchell Heating & Air
                 </p>
+                <ul>
+                    <li>
+                        Maintained quality assurance and customer satisfaction objectives.
+                    </li>
+                    <li>
+                        Executed diagnostics, troubleshooting and evaluations on HVAC units.
+                    </li>
+                    <li>
+                        Organized work to meet demanding production goals.
+                    </li>
+                    <li>
+                        Completed job reports and logs immediately following service calls.
+                    </li>
+                </ul>
+                <p>
+                    Delivery Driver - Sharon's linen and laundry service
+                </p>
+                <ul>
+                    <li>
+                        Successfully avoided time delivery delays by carefully planning best routes.
+                    </li>
+                    <li>
+                        Fostered positive working relationships with regular customers.
+                    </li>
+                    <li>
+                        Verified daily deliveries against shipping instructions before delivering to customers
+                    </li>
+                </ul>
+                <p>
+                    Deli Worker - Ingles Markets
+                </p>
+                <ul>
+                    <li>
+                        Carefully prepared orders by slicing, weighing and packaging cheeses and meats
+                        and accurately calculated prices.
+                    </li>
+                    <li>
+                        Cleaned and sanitized dishes and utensils, consistently keeping adequate supplies
+                        on hand for expected customer loads.
+                    </li>
+                    <li>
+                        Listened carefully to customer instructions and prepared orders according to those
+                        preferences.
+                    </li>
+                </ul>
+                <p>
+                    Cashier - Poppy Seeds Bagels
+                </p>
+                <ul>
+                    <li>
+                        Operated cash register for cash, check and credit card transactions with excellent accuracy levels.
+                    </li>
+                    <li>
+                        Worked flexible schedule and extra shifts to meet business needs.
+                    </li>
+                    <li>
+                        Restocked and organized merchandise in front lanes.
+                    </li>
+                    <li>
+                        Answered questions about store policies and addressed customer concerns.
+                    </li>
+                    <li>
+                        Worked closely with shift manager to solve problems and handle customer concerns.
+                    </li>
+                </ul>
+                <p>
+                    Line Cook - Smoke BBQ & Grill
+                </p>
+                <ul>
+                    <li>
+                        Prepared multiple orders simultaneously during peak periods with high accuracy rate maximizing customer satisfaction and repeat business.
+                    </li>
+                    <li>
+                        Set up and prepared cooking supplies and workstations during opening and closing to
+                        maximize productivity.
+                    </li>
+                    <li>
+                        Created identical dishes numerous times daily with consistent care, attention to detail and quality.
+                    </li>
+                </ul>
             </section>
         </div>
     )
